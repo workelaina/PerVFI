e946c46934df80a786f2c272b978f739012d1a03
elaina <userelaina@pm.me>
2024-11-25 11:23:30 +0000

    :sparkles: infer_img

--- infer_img.py
+++ infer_img.py
@@ -1,10 +1,10 @@
 import cv2
+import torch
+from torch import Tensor
 from build_models import build_model
 
 l = cv2.imread('test/133.png', cv2.IMREAD_UNCHANGED)
 r = cv2.imread('test/253.png', cv2.IMREAD_UNCHANGED)
-
-print(l.size(), r.size())
 
 # video = 'test/Elysia1.png'
 # capture = cv2.VideoCapture(video)
@@ -15,6 +15,10 @@ print(l.size(), r.size())
 # succ, l = capture.read()
 # succ, r = capture.read()
 
+l = torch.Tensor(l)
+r = torch.Tensor(r)
+print(l.size(), r.size())
+
 model, infer = build_model('none+PerVFI')
 res = infer(l, r)
-cv2.imwrite('test/res.png', res)
+cv2.imwrite('test/res.png', res.numpy())
